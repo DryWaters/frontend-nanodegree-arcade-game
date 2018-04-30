b76dcd640b54537ea1216a5eff280a5e599311fe
Daniel Waters <danwater1@gmail.com>
2018-04-30 11:22:31 -0700

    Created movement for enemy

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = -100;
+    this.y = 50;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +16,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    const movement = 200 * dt;
+    this.x += movement;
+    if (this.x >= 550) {
+        this.x = -100;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +31,50 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.x = 200;
+    this.y = 375;
+    this.sprite = 'images/char-boy.png';
+}
 
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(keyNum) {
+    console.log('x is ', this.x, 'y is ', this.y);
+    switch(keyNum) {
+        case 'left': {
+            this.x -= 100;
+            return;
+        }
+        case 'right': {
+            this.x += 100;
+            return;
+        }
+        case 'up': {
+            this.y -= 83;
+            return;
+        }
+        case 'down': {
+            this.y += 83;
+            return;
+        }
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var enemy1 = new Enemy();
+var allEnemies = [enemy1];
+
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
