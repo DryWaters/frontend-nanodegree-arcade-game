ec31b489c35d060cad0eb7d17f61ecb7fb9fcecf
Daniel <danwater1@gmail.com>
2018-04-30 20:42:08 -0700

    Added comments to code

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
 
-
-; (function (global) {
+// IIFE to hide from global scope
+;(function (global) {
 
   // Enemies our player must avoid
   var Enemy = function () {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // these values are the constant Y values
+    // that are equal to the values of the
+    // rock blocks
     this.enemyStartY = [143, 226, 309];
     this.enemyStartX = -100;
     this.sprite = 'images/enemy-bug.png';
     this.initEnemy();
   };
 
-  // Update the enemy's position, required method for game
   // Parameter: dt, a time delta between ticks
   Enemy.prototype.update = function (dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     this.x += this.speed * dt;
+
+    // if enemy runs off the screen
+    // then reset themselves back to the left
     if (this.x >= 500) {
       this.initEnemy();
     }
+    // if player collides remove a heart
+    // and reset them back to beginning
     if (this.hasCollision()) {
       player.reset();
       removeHeart();
@@ -32,6 +32,9 @@
   };
 
   // check collision with player and enemy
+  // by finding the absolute difference between
+  // the two characters which sould be  
+  // less than half the width/height of a block
   Enemy.prototype.hasCollision = function () {
     if (
       Math.abs(this.x - player.x) < 60 &&
@@ -42,43 +45,50 @@
     }
   }
 
-  // Set the enemy starting location and speed
+  // Set the enemy random starting location and speed
   Enemy.prototype.initEnemy = function () {
+    // the base speed is 200
     this.speed = Math.floor(Math.random() * 200) + 200;
+    
+    // multiple by -1 to move the enemies slightly off the screen
     this.x = (Math.floor(Math.random() * 300) * -1) + this.enemyStartX;
     this.y = this.enemyStartY[Math.floor(Math.random() * this.enemyStartY.length)];
   }
 
-  // Draw the enemy on the screen, required method for game
+  // Draw the enemy on the screen
   Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   };
 
-  // Now write your own player class
-  // This class requires an update(), render() and
-  // a handleInput() method.
+  // Player object
   var Player = function () {
     this.sprite = 'images/char-boy.png';
     this.init();
   }
 
+  // reset the player if they collide
+  // with enemy or make it to the end
   Player.prototype.init = function () {
     this.x = 202;
     this.y = 458;
   }
 
+  // if player makes it to the end
+  // then recreate the gems
   Player.prototype.reset = function () {
     gems.forEach(function (gem) {
       gem.init();
     });
-    this.x = 202;
-    this.y = 458;
+    this.init();
   };
 
   Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
 
+  // handles the keyboard input
+  // adding 10 to the score if they
+  // make it to the end 
   Player.prototype.handleInput = function (keyNum) {
     switch (keyNum) {
       case 'left': {
@@ -111,6 +121,9 @@
     }
   }
 
+  // Heart object used to display
+  // the number of lives the player has left
+  // They start with 3 lives.
   var Heart = function (x) {
     this.x = x;
     this.y = 40;
@@ -121,6 +134,10 @@
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
 
+  // Score object used to display
+  // the players current score
+  // This object also keeps track of how 
+  // many gems they have collected.
   var Score = function () {
     this.score = 0;
     this.greenGems = 0;
@@ -128,6 +145,8 @@
     this.orangeGems = 0;
   }
 
+  // reset function if player wants
+  // to play again
   Score.prototype.init = function () {
     this.score = 0;
     this.greenGems = 0;
@@ -144,6 +163,10 @@
     this.score += score;
   }
 
+  // Gems object that is used to
+  // increase the players score
+  // they randomly appear and move
+  // around randomly
   var Gem = function () {
     this.inPlay = true;
     this.init();
@@ -161,6 +184,15 @@
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
 
+  // Gems are randomly moved if 1/1000th chance
+  // on every update
+  // Gem points are setup as 
+  // green = 200
+  // blue = 100
+  // orange = 50
+  // If player makes it across to the
+  // other side the gems reappear by 
+  // updating the "inPlay" property
   Gem.prototype.update = function () {
     if (this.inPlay) {
       if (Math.floor(Math.random() * 1000) === 500) {
@@ -182,6 +214,8 @@
     }
   }
 
+  // if player picks up the gem
+  // do not allow it to reappear
   Gem.prototype.hasCollision = function () {
     if (
       Math.abs(this.x - player.x) < 50 &&
@@ -191,9 +225,11 @@
     }
   }
 
+  // remove the gem by moving it off the page
+  // instead of recreating the object
   Gem.prototype.removeGem = function () {
     this.x = 1000;
-    inPlay = false;
+    this.inPlay = false;
   }
 
   // This listens for key presses and sends the keys to your
@@ -218,6 +254,12 @@
     }
   }
 
+  // function for when player runs out of hearts
+  // 1.  it updates the local storage high score (if needed)
+  // 2.  stops the updating of the game by changing the 
+  //     isRunning variable within the "Engine"
+  // 3.  it removes gems and enemies
+  // 4.  Displays modal after updating the score/highscore
   function gameOver() {
     processHighScore();
     allEnemies.forEach(function (enemy) {
@@ -232,12 +274,17 @@
     stopGame();
   }
 
+  // displays the number of gems the player collected on the
+  // game over modal
   function displayGems() {
     document.querySelector('.modal__gems__green').textContent = score.greenGems;
     document.querySelector('.modal__gems__blue').textContent = score.blueGems;
     document.querySelector('.modal__gems__orange').textContent = score.orangeGems;
   }
 
+  // function that updates the local storage high score (if available)
+  // depending on if the player beats the high score, a different message
+  // is displayed
   function processHighScore() {
     if (localStorage === undefined) {
       document.querySelector('.modal__message').textContent = 'Sorry, local storage is not enabled';
@@ -245,6 +292,8 @@
     }
 
     let currentHighScore;
+
+    // check if the highscore variable has not be saved yet (first time playing)
     if (localStorage.getItem('highScore') !== null) {
       currentHighScore = Number.parseInt(localStorage.getItem('highScore'));
     } else {
@@ -270,16 +319,19 @@
 
   }
 
+  // resets the hearts back to 3 when player wants to play again
   function resetHearts() {
     hearts = [heart1, heart2, heart3];
   }
 
+  // resets the gems when player wants to play again
   function resetGems() {
     gems.forEach(function (gem) {
       gem.init();
     });
   }
 
+  // starts the game back when player wants to play again
   function resetGame() {
     score.score = 0;
     player.init();
@@ -290,9 +342,12 @@
     reset();
   }
 
+  // Objects created
   const enemy1 = new Enemy();
   const enemy2 = new Enemy();
   const enemy3 = new Enemy();
+
+  // The argument is the Heart's X value
   const heart1 = new Heart(400);
   const heart2 = new Heart(300);
   const heart3 = new Heart(200);
@@ -300,6 +355,9 @@
   const gem2 = new Gem();
   const gem3 = new Gem();
 
+  // objects that are shown to the global name space
+  // this access is needed by the "Engine" to properly
+  // render/update the objects
   global.hearts = [heart1, heart2, heart3];
   global.allEnemies = [enemy1, enemy2, enemy3];
   global.gems = [gem1, gem2, gem3];
@@ -309,6 +367,9 @@
 
 })(this);
 
+// attach a listener that will randomly change the background
+// color of the modal on hover
+// (colors are the colors of the gems)
 (function randomButtonColor () {
   let button = document.querySelector('.modal__button');
   button.addEventListener('mouseover', function(event) {
